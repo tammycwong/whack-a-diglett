1f0699c6d0e31f41c579275b75174f8b19ff6353
Tammy <tammycwong@gmail.com>
2021-07-22 16:33:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelectorAll('.time-left')
+let score = document.querySelectorAll('#score')
+
+let result = 0
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+    hitPosition = randomPosition.id
+}
+
