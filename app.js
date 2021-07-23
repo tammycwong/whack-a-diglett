52a5b4c53d5b3ff7b6527b056c36379e2863d5d5
Tammy <tammycwong@gmail.com>
2021-07-23 12:39:29 -0400

    diglett overlay

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
-const square = document.querySelectorAll('.square')
-const mole = document.querySelectorAll('.mole')
-const timeLeft = document.querySelectorAll('.time-left')
-let score = document.querySelectorAll('#score')
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
 
 let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
 
 function randomSquare() {
-    square.forEach(className => {
-        className.classList.remove('mole')
-    })
-    let randomPosition = square[Math.floor(Math.random() * 9)]
-    randomPosition.classList.add('mole')
+  squares.forEach(square => {
+    square.classList.remove('mole')
+  })
 
-    hitPosition = randomPosition.id
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('mole')
+
+  hitPosition = randomSquare.id
 }
 
+// const image = document.createElement('img')
+// image.src = 'hole.png'
+// document.querySelector('.square').appendChild(image)
+
+squares.forEach(square => {
+  square.addEventListener('mousedown', () => {
+    if (square.id == hitPosition) {
+      result++
+      score.textContent = result
+      hitPosition = null
+    }
+  })
+})
+
+function moveMole() {
+  timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+function countDown() {
+ currentTime--
+ timeLeft.textContent = currentTime
+
+ if (currentTime == 0) {
+   clearInterval(countDownTimerId)
+   clearInterval(timerId)
+   alert('GAME OVER! Your final score is ' + result)
+ }
+
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
